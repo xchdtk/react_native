2ea32b1d41e7ec40efe39466a8c20b246ea50505
kimjinsoo <1004kjs_@naver.com>
2021-12-25 15:14:09 +0900

    styled components 학습

--- rn-style/src/App.js
+++ rn-style/src/App.js
@@ -1,22 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Shadow from "./Shadow";
+import styled, { css } from "styled-components/native";
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #e3e3e3;
+    align-items: center;
+    justify-content: center;
+`;
+
+const cssText = css`
+    font-size: 40px;
+    font-weight: 600;
+`;
+
+const StyledText = styled.Text`
+    ${cssText}
+    color: blue;
+`;
+
+const ErrorText = styled.Text`
+    ${cssText}
+    color: red;
+`;
+
+const StyledButton = styled.Button``;
 
 export default function App() {
     return (
-        <View style={styles.container}>
+        <Container>
             <StatusBar style="auto" />
-            <Shadow />
-        </View>
+            <StyledText>style componenets</StyledText>
+            <ErrorText>error !!</ErrorText>
+            <StyledButton title="사랑해" onPress={() => alert("더 사랑해")} />
+        </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
